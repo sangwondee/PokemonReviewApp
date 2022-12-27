1c62eec3041d4983a1ea4d7414522943726686b4
sangwondee <sangwondee@gmail.com>
2022-12-27 23:25:13 +0700

    Can get pokemon lists

--- PokemonReviewApp/Data/DataContext.cs
+++ PokemonReviewApp/Data/DataContext.cs
@@ -20,7 +20,7 @@ public class DataContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Country> Countries { get; set; }
     public DbSet<Owner> Owners { get; set; }
-    public DbSet<Pokemon> Pokemon { get; set; }
+    public DbSet<Pokemon?> Pokemon { get; set; }
     public DbSet<PokemonOwner> PokemonOwners { get; set; }
     public DbSet<PokemonCategory> PokemonCategories { get; set; }
     public DbSet<Review> Reviews { get; set; }
--- PokemonReviewApp/Repository/PokemonRepository.cs
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -17,4 +17,29 @@ public class PokemonRepository : IPokemonRepository
     {
         return _context.Pokemon.OrderBy(p => p.Id).ToList();
     }
+
+    public Pokemon GetPokemon(int id)
+    {
+        return _context.Pokemon.FirstOrDefault(p => p.Id == id);
+    }
+
+    public Pokemon? GetPokemon(string name)
+    {
+        return _context.Pokemon.FirstOrDefault(p => p.Name == name);
+    }
+
+    public decimal GetPokemonRating(int pokeId)
+    {
+        var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+
+        if (!review.Any())
+            return 0;
+
+        return ((decimal)review.Sum(r => r.Rating) / review.Count());
+    }
+
+    public bool PokemonExists(int pokeId)
+    {
+        throw new NotImplementedException();
+    }
 }
